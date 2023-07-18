80528df929d3c4eb2e96c9b89f99d399042929e3
saeed azarafrooz <saeed_azarafrooz@yahoo.com>
2023-07-19 01:01:44 +0330

    Image Loader3

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="../public/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
