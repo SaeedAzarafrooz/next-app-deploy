5fa969e7d10f0b318ee71aaaed9246d08aaf6b98
saeed azarafrooz <saeed_azarafrooz@yahoo.com>
2023-07-19 00:50:09 +0330

    Image Loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    images: {
+        loader: 'custom',
+        loaderFile: './app/image.js',
+      },
 }
 
 module.exports = nextConfig
